32cd285bc7e3e551862dbfef99c653f59ae5c54b
SebastianMora117 <129330647+SebastianMora117@users.noreply.github.com>
2023-09-18 11:00:24 -0500

    Update cancelacion.php

--- cancelacion/cancelacion.php
+++ cancelacion/cancelacion.php
@@ -1,26 +1,34 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-include_once('conexion.php');
 
-    $casillero = $_POST["casillero"];
+include('../PHP/Funciones.php');
 
-    // Paso 3: Ejecuta una consulta SQL DELETE para eliminar la fila correspondiente
-    $sql = "DELETE FROM lockers WHERE id_locker = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $casillero);
+InicioSesion();
+Inactividad(1800);
+if (isset($_GET['cerrar_sesion'])) {
+    cerrarSesion();
+}
 
-    if ($stmt->execute()) {
-        echo "Reserva del casillero $casillero ha sido cancelada con éxito.";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Verificamos si se envió un número de casillero válido
+    if (isset($_POST['casillero']) && is_numeric($_POST['casillero'])) {
+        $casillero = $_POST['casillero'];
+
+        include_once('conexion.php');
+
+
+        // Consulta para cancelar la reserva del casillero
+        $sql = "DELETE FROM lockers WHERE id_locker = $casillero";
+
+        if ($conexion->query($sql) === TRUE) {
+            echo "Reserva cancelada correctamente";
+        } else {
+            echo "Error al cancelar la reserva: " . $conexion->error;
+        }
+
+        $conexion->close();
     } else {
-        echo "Error al cancelar la reserva del casillero: " . $stmt->error;
+        echo "El número de casillero no es válido";
     }
-
-    // Cierra la conexión
-    $stmt->close();
-    $conn->close();
-
-
-    mysqli_close($conexion);
 }
 
 ?>
