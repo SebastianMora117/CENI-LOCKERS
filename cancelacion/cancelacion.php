c41b5cd46e6a73e259b897b86d0c328ba64f8853
SebastianMora117 <129330647+SebastianMora117@users.noreply.github.com>
2023-09-18 10:07:46 -0500

    Update cancelacion.php

--- cancelacion/cancelacion.php
+++ cancelacion/cancelacion.php
@@ -1,28 +1,24 @@
-
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 include_once('conexion.php');
 
-$porque=$_POST['porque'];
-$casillero=$_POST['casillero'];
-$nomestu=$_POST['nom']; 
+    $casillero = $_POST["casillero"];
 
-if ($porque == "" || $casillero == "" || $nomestu == "") {
-    echo "<script>alert('Debes tener los campos completados');
-    window.location.href = '../cancelacion/cancelacionn.php';</script>";
-    exit();
-}
-$consulta = "INSERT INTO cancelacion (razon,nombreest,numeroloker) VALUES ('$porque', '$nomestu', '$casillero')";
+    // Paso 3: Ejecuta una consulta SQL DELETE para eliminar la fila correspondiente
+    $sql = "DELETE FROM lockers WHERE id_locker = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $casillero);
 
-    if (mysqli_query($conexion, $consulta)) {
-        echo "<script>alert('cancelacion realizada');
-        window.location.href = '../cancelacion/cancelacionn.php';</script>";
-        exit();
+    if ($stmt->execute()) {
+        echo "Reserva del casillero $casillero ha sido cancelada con éxito.";
     } else {
-        echo "<script>alert('Error al cancelar: " . mysqli_error($conexion) . "');
-        window.location.href = '../cancelacion/cancelacionn.php';</script>";
-        exit();
+        echo "Error al cancelar la reserva del casillero: " . $stmt->error;
     }
+
+    // Cierra la conexión
+    $stmt->close();
+    $conn->close();
+
 
     mysqli_close($conexion);
 }
