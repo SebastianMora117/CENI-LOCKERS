14c879f6b2333b8bcb79f908f9c144de99dd59d0
JuanMendez12 <136756922+JuanMendez12@users.noreply.github.com>
2023-09-17 21:40:12 -0500

    Update reserva.php

--- mapa/reserva.php
+++ mapa/reserva.php
@@ -1,21 +1,25 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-include_once('conexion.php');
+    include_once('conexion.php');
 
-$lockerSelec=$_POST['locker'];
+    $lockerSelec = $_POST['locker'];
 
+    if ($lockerSelec == "") {
+        echo "<script>alert('Debes seleccionar un casillero');
+        window.location.href = '../mapa/index.php';</script>";
+        exit();
+    }
 
-if ($lockerSelec == "") {
-    echo "<script>alert('Debes seleccionar un casillero');
-    window.location.href = '../mapa/index.php';</script>";
-    exit();
-}
-$consulta = "INSERT INTO lockers (id_locker) VALUES ('$lockerSelec')";
+    $consulta = "INSERT INTO lockers (id_locker, reservation_time, expiration_time) VALUES ('$lockerSelec', NOW(), DATE_ADD(NOW(), INTERVAL 1 MINUTE))";
 
     if (mysqli_query($conexion, $consulta)) {
         echo "<script>alert('Reserva realizada');
         window.location.href = '../mapa/index.php';</script>";
+
+        // Eliminar la reserva después de 1 minuto
+       
+
         exit();
     } else {
         echo "<script>alert('Error al reservar: " . mysqli_error($conexion) . "');
@@ -25,5 +29,4 @@ $consulta = "INSERT INTO lockers (id_locker) VALUES ('$lockerSelec')";
 
     mysqli_close($conexion);
 }
-
 ?>
