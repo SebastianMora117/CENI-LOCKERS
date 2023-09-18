a63e6f6acb8e62a2548e65c74e2f3e1c4c549232
santiagoma7 <129330790+santiagoma7@users.noreply.github.com>
2023-09-17 21:48:31 -0500

    Update reserva.php

--- mapa/reserva.php
+++ mapa/reserva.php
@@ -1,9 +1,17 @@
-
 <?php
+session_start(); // Iniciar la sesión si aún no está iniciada
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include_once('conexion.php');
 
     $lockerSelec = $_POST['locker'];
+
+    // Verificar si ya se seleccionó un casillero
+    if (isset($_SESSION['locker_seleccionado'])) {
+        echo "<script>alert('Ya has seleccionado un casillero');
+        window.location.href = '../mapa/index.php';</script>";
+        exit();
+    }
 
     if ($lockerSelec == "") {
         echo "<script>alert('Debes seleccionar un casillero');
@@ -11,15 +19,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    $consulta = "INSERT INTO lockers (id_locker, reservation_time, expiration_time) VALUES ('$lockerSelec', NOW(), DATE_ADD(NOW(), INTERVAL 1 MINUTE))";
+    // Verificar si el casillero ya está ocupado
+    $consulta_disponibilidad = "SELECT id_locker FROM lockers WHERE id_locker = '$lockerSelec'";
+    $resultado = mysqli_query($conexion, $consulta_disponibilidad);
+
+    if (mysqli_num_rows($resultado) > 0) {
+        echo "<script>alert('El casillero seleccionado no está disponible');
+        window.location.href = '../mapa/index.php';</script>";
+        exit();
+    }
+
+    $consulta = "INSERT INTO lockers (id_locker) VALUES ('$lockerSelec')";
 
     if (mysqli_query($conexion, $consulta)) {
+        // Registrar la selección en la variable de sesión
+        $_SESSION['locker_seleccionado'] = true;
+
         echo "<script>alert('Reserva realizada');
         window.location.href = '../mapa/index.php';</script>";
-
-        // Eliminar la reserva después de 1 minuto
-       
-
         exit();
     } else {
         echo "<script>alert('Error al reservar: " . mysqli_error($conexion) . "');
