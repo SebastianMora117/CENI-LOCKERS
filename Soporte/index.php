5b277625f961aa7cb4d455037f7ec3fde51282ed
JuanMendez12 <136756922+JuanMendez12@users.noreply.github.com>
2023-09-20 23:07:34 -0500

    Update index.php

--- Soporte/index.php
+++ Soporte/index.php
@@ -26,9 +26,11 @@ if (isset($_GET['cerrar_sesion'])) {
         <div class="container">
             <p class="logo">CeniLockers!</p>
             <nav>
-                <a href="../reportes/index.php">Reportes</a>
-                <a href="../mapa/index.php">Reserva ya</a>
+                <a href="../paginainicio/index.php">Inicio</a>
+                <a href="../mapa/index.php">Reserva</a>
                 <a href="../cancelacion/cancelacionn.php">Cancelación</a>
+                <a href="../reportes/index.php">Reportes</a>
+                <a href="../Soporte/index.php">Soporte</a>
                 <a href="?cerrar_sesion=1">Cerrar Sesión</a>
             </nav>
         </div>
